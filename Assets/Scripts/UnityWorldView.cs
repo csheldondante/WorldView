95a9e4966f49cef7757ab6360008911e0afd404c
csheldondante <csheldondante@gmail.com>
2019-05-21 20:58:52 -0400

    continued updating world sim integration

--- Assets/Scripts/UnityWorldView.cs
+++ Assets/Scripts/UnityWorldView.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnityWorldView : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+namespace CSD{
+	public class UnityWorldView : MonoBehaviour, IWorldView<UnityViewComponent> {
+		private WorldSim<UnityViewComponent, UnityWorldView> worldSim;
+		private HashSet<UnityViewComponent> entityViews = new HashSet<UnityViewComponent>();
+
+		// Use this for initialization
+		void Start () {
+			//check session data from previous run
+			//if there was an in progress game load the world state and set any asset flags for view assets
+			//start concurrent loading of view assets for menu
+			//display the main menu
+			//start concurrent loading of game assets
+			//wait for user input to change the game state and transition to corresponding loading screen/game world
+
+			//TEST CODE
+			worldSim = new WorldSim<UnityViewComponent, UnityWorldView>(this);
+			WorldSim<UnityViewComponent,UnityWorldView>.GetDistance (null, null);
+			//END TEST CODE
+		}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		// Update is called once per frame
+		void Update () {
+			if (worldSim == null)
+				return;
+			worldSim.Tick (Time.deltaTime);
+		}
+
+		public UnityViewComponent GetView(IEntity entity){
+			var view = entity.GetComponent<UnityViewComponent> ();
+			if (view != null)
+				return view;
+			var newView = CreateView (entity);
+			entity.AddComponent (newView);
+			return newView;
+		}
+
+		private UnityViewComponent CreateView(IEntity entity){
+			return null;
+		}
+		public float GetDistance(IEntity obj1, IEntity obj2){
+			return float.NaN;
+		}
+
+		public Objective GetRangeObjective(IEntity obj1, IEntity obj2){
+			return null;
+		}
+		public WorldEvent MoveTo(IEntity entity, IEntity target){
+			return null;
+		}
 	}
 }
---
+++ Assets/UnityViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CSD{
+
+	public class UnityViewComponent : MonoBehaviour, IViewComponent {
+		private IEntity entity;
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+
+		public bool SetEntity(IEntity entity){
+			this.entity = entity;
+			RefreshFromEntity ();
+			return true;
+		}
+
+		public IEntity GetEntity(){
+			return this.entity;
+		}
+
+		private void RefreshFromEntity(){
+		}
+	}
+}
